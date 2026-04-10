41ce8a82784334a0b1eda6f4f2c7f9036256056c
banemk2339-ai <banemk2339@gmail.com>
2026-04-10 20:20:55 +0200

    update

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const { spawn } = require("child_process");
 const log = require("./logger/log.js");
+const http = require("http"); // Add this
+
+// 1. Create a simple server to satisfy Render's port requirement
+const port = process.env.PORT || 10000;
+http.createServer((req, res) => {
+    res.write("Bot is running!");
+    res.end();
+}).listen(port);
 
 function startProject() {
-	const child = spawn("node", ["Goat.js"], {
-		cwd: __dirname,
-		stdio: "inherit",
-		shell: true
-	});
+    // 2. Start the actual bot
+    const child = spawn("node", ["Goat.js"], {
+        cwd: __dirname,
+        stdio: "inherit",
+        shell: true
+    });
 
-	child.on("close", (code) => {
-		if (code == 2) {
-			log.info("Restarting Project...");
-			startProject();
-		}
-	});
+    child.on("close", (code) => {
+        if (code == 2) {
+            log.info("Restarting Project...");
+            startProject();
+        }
+    });
 }
 
 startProject();
